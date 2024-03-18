a73f1ff79db0a200c7a31383e9b186f9af249e1f
skapskap <skaprange@gmail.com>
2024-03-18 17:50:10 -0300

    synchronous fetching

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+// Synchronous Fetching - Result: 300.64ms
+
 func main()  {
-	
+	start := time.Now()
+	username := fetchUser()
+	likes := fetchUserLikes(username)
+	match := fetchUserMatch(username)
+
+	fmt.Println("likes: ", likes)
+	fmt.Println("match: ", match)
+	fmt.Println("took: ", time.Since(start))
+}
+
+func fetchUser() string {
+	time.Sleep(time.Millisecond * 100)
+
+	return "Lucas"
+}
+
+func fetchUserLikes(username string) int {
+	time.Sleep(time.Millisecond * 100)
+
+	return 11
+}
+
+func fetchUserMatch(username string) string {
+	time.Sleep(time.Millisecond * 100)
+
+	return "Sabrina"
 }
