07a406961f716e61f3e5b405e456b9611f2870f5
skapskap <skaprange@gmail.com>
2024-03-18 19:00:05 -0300

    asynchronous fetching

--- main.go
+++ main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-// Synchronous Fetching - Result: 300.64ms
+// Synchronous Fetching - Result: 200.56ms
 
 func main()  {
 	start := time.Now()
 	username := fetchUser()
-	likes := fetchUserLikes(username)
-	match := fetchUserMatch(username)
+	respCh := make(chan any, 2)
+	wg := &sync.WaitGroup{}
 
-	fmt.Println("likes: ", likes)
-	fmt.Println("match: ", match)
+	wg.Add(2)
+
+	go fetchUserLikes(username, respCh, wg)
+	go fetchUserMatch(username, respCh, wg)
+
+	wg.Wait() // Block the execution until 2 wg.Done() are called
+	close(respCh) // Channel closing before the goroutines finish their task - use waitgroup
+
+	for resp := range respCh {
+		fmt.Println("resp: ", resp)
+	}
+
+	// fmt.Println("likes: ", likes)
+	// fmt.Println("match: ", match)
 	fmt.Println("took: ", time.Since(start))
 }
 
@@ -24,14 +37,47 @@ func fetchUser() string {
 	return "Lucas"
 }
 
-func fetchUserLikes(username string) int {
+func fetchUserLikes(username string, respCh chan any, wg *sync.WaitGroup) {
 	time.Sleep(time.Millisecond * 100)
 
-	return 11
+	respCh <- 11
+	wg.Done()
 }
 
-func fetchUserMatch(username string) string {
+func fetchUserMatch(username string, respCh chan any, wg *sync.WaitGroup) {
 	time.Sleep(time.Millisecond * 100)
 
-	return "Sabrina"
+	respCh <- "Sabrina"
+	wg.Done()
 }
+
+// Synchronous Fetching - Result: 300.64ms
+
+// func main()  {
+// 	start := time.Now()
+// 	username := fetchUser()
+// 	likes := fetchUserLikes(username)
+// 	match := fetchUserMatch(username)
+
+// 	fmt.Println("likes: ", likes)
+// 	fmt.Println("match: ", match)
+// 	fmt.Println("took: ", time.Since(start))
+// }
+
+// func fetchUser() string {
+// 	time.Sleep(time.Millisecond * 100)
+
+// 	return "Lucas"
+// }
+
+// func fetchUserLikes(username string) int {
+// 	time.Sleep(time.Millisecond * 100)
+
+// 	return 11
+// }
+
+// func fetchUserMatch(username string) string {
+// 	time.Sleep(time.Millisecond * 100)
+
+// 	return "Sabrina"
+// }
