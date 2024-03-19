be9d40e779dc91345a4ec5980a32f3c882195514
skapskap <skaprange@gmail.com>
2024-03-18 23:11:35 -0300

    refactor: sync && async code running together

--- main.go
+++ main.go
@@ -6,78 +6,81 @@ import (
 	"time"
 )
 
-// Synchronous Fetching - Result: 200.56ms
+func main() {
+	// Synchronous Fetching - Result: 300.69ms
+	startSync := time.Now()
+	syncUsername := fetchUserSync()
+	syncLikes := fetchUserLikesSync(syncUsername)
+	syncMatch := fetchUserMatchSync(syncUsername)
 
-func main()  {
-	start := time.Now()
-	username := fetchUser()
+	fmt.Println("Synchronous Fetching:")
+	fmt.Println("Username:", syncUsername)
+	fmt.Println("Likes:", syncLikes)
+	fmt.Println("Match:", syncMatch)
+	fmt.Println("Synchronous Fetching took:", time.Since(startSync))
+
+	fmt.Println()
+
+	// Asynchronous Fetching - Result: 200.42ms
+	startAsync := time.Now()
+	asyncUsername := fetchUserAsync()
 	respCh := make(chan any, 2)
 	wg := &sync.WaitGroup{}
 
 	wg.Add(2)
 
-	go fetchUserLikes(username, respCh, wg)
-	go fetchUserMatch(username, respCh, wg)
+	go fetchUserLikesAsync(asyncUsername, respCh, wg)
+	go fetchUserMatchAsync(asyncUsername, respCh, wg)
 
-	wg.Wait() // Block the execution until 2 wg.Done() are called
-	close(respCh) // Channel closing before the goroutines finish their task - use waitgroup
+	wg.Wait()
+	close(respCh)
 
+	var asyncLikes int
+	var asyncMatch string
 	for resp := range respCh {
-		fmt.Println("resp: ", resp)
+		switch v := resp.(type) {
+		case int:
+			asyncLikes = v
+		case string:
+			asyncMatch = v
+		}
 	}
 
-	// fmt.Println("likes: ", likes)
-	// fmt.Println("match: ", match)
-	fmt.Println("took: ", time.Since(start))
+	fmt.Println("Asynchronous Fetching:")
+	fmt.Println("Username:", asyncUsername)
+	fmt.Println("Likes:", asyncLikes)
+	fmt.Println("Match:", asyncMatch)
+	fmt.Println("Asynchronous Fetching took:", time.Since(startAsync))
 }
 
-func fetchUser() string {
+func fetchUserSync() string {
 	time.Sleep(time.Millisecond * 100)
-
 	return "Lucas"
 }
 
-func fetchUserLikes(username string, respCh chan any, wg *sync.WaitGroup) {
+func fetchUserLikesSync(username string) int {
 	time.Sleep(time.Millisecond * 100)
+	return 11
+}
 
+func fetchUserMatchSync(username string) string {
+	time.Sleep(time.Millisecond * 100)
+	return "Sabrina"
+}
+
+func fetchUserAsync() string {
+	time.Sleep(time.Millisecond * 100)
+	return "Lucas"
+}
+
+func fetchUserLikesAsync(username string, respCh chan any, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 100)
 	respCh <- 11
 	wg.Done()
 }
 
-func fetchUserMatch(username string, respCh chan any, wg *sync.WaitGroup) {
+func fetchUserMatchAsync(username string, respCh chan any, wg *sync.WaitGroup) {
 	time.Sleep(time.Millisecond * 100)
-
 	respCh <- "Sabrina"
 	wg.Done()
 }
-
-// Synchronous Fetching - Result: 300.64ms
-
-// func main()  {
-// 	start := time.Now()
-// 	username := fetchUser()
-// 	likes := fetchUserLikes(username)
-// 	match := fetchUserMatch(username)
-
-// 	fmt.Println("likes: ", likes)
-// 	fmt.Println("match: ", match)
-// 	fmt.Println("took: ", time.Since(start))
-// }
-
-// func fetchUser() string {
-// 	time.Sleep(time.Millisecond * 100)
-
-// 	return "Lucas"
-// }
-
-// func fetchUserLikes(username string) int {
-// 	time.Sleep(time.Millisecond * 100)
-
-// 	return 11
-// }
-
-// func fetchUserMatch(username string) string {
-// 	time.Sleep(time.Millisecond * 100)
-
-// 	return "Sabrina"
-// }
